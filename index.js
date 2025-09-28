9cbade941d70903e55426e11bc910fda4684f552
Ahamed Atheef <ahdatheef451@gmail.com>
2025-09-28 19:13:40 +0530

    new

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-import cors from "cors";
-app.use(cors({
-    origin: "*", // or "https://your-netlify-app.netlify.app"
-    credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // routes
@@ -32,8 +27,8 @@ app.listen(port, () => {
     console.log(`Server is running on http://${port}`);
 });
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("database connected")
-}).catch(() => {
+}).catch(()=>{
     console.log("failed to connect database")
 })
